e742e0812e655bfa207e09f0fe100585c2a87577
Vu Duc <ducwebdev@gmail.com>
2025-04-20 16:19:25 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className='text-2xl font-bold text-red-50'>
       <p>Code base 1</p>
-      <p>Testing gh action</p>
+      <p>Testing gh action 2</p>
     </div>
   )
 }
