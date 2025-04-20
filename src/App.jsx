2bb57cca53a5a9e88b30008dc4e13d47a54cb2a7
Vu Duc <ducwebdev@gmail.com>
2025-04-20 16:09:47 +0700

    chore: test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div className='text-2xl font-bold text-red-50'>Code base 1</div>
+  return (
+    <div className='text-2xl font-bold text-red-50'>
+      <p>Code base 1</p>
+      <p>Testing gh action</p>
+    </div>
+  )
 }
 
 export default App
