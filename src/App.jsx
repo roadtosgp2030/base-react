d566dde881bc0cfd6c65bdbbfb72a6c819cbca6d
Vu Duc <ducwebdev@gmail.com>
2025-04-20 16:26:55 +0700

    chore: remove redundant code and update something

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className='text-2xl font-bold text-red-50'>
+    <div className='text-2xl font-bold text-red-600'>
       <p>Code base 1</p>
-      <p>Testing gh action 2</p>
+      <p>Testing gh action 3</p>
     </div>
   )
 }
